20ccca201072acf00c6c4c872285ea57cd19ae56
ika2kan-fjyu <yu3horikawa@gmail.com>
2026-01-14 19:46:02 +0900

    ナビゲーションを画面下に固定表示

--- src/layouts/MobileLayout.jsx
+++ src/layouts/MobileLayout.jsx
@@ -4,8 +4,10 @@ import { Navigation } from '../components/Navigation';
 export function MobileLayout({ children }) {
     return (
         <div className="min-h-screen bg-bg-base flex justify-center">
-            <div className="w-full max-w-md bg-white min-h-screen shadow-2xl overflow-x-hidden relative pb-24 animate-fade-in">
-                {children}
+            <div className="w-full max-w-md bg-white min-h-screen shadow-2xl relative">
+                <div className="pb-24 animate-fade-in">
+                    {children}
+                </div>
                 <Navigation />
             </div>
         </div>
