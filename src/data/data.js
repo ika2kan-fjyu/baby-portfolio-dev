02d0b3f4443de173083d6f69a4d2efdd8fbf41d4
ika2kan-fjyu <yu3horikawa@gmail.com>
2026-01-15 17:01:28 +0900

    データを反映

---
+++ src/data/data.js
@@ -0,0 +1,504 @@
+export const profileData = {
+    name: "周 (Amane)",
+    birthDate: "2025-09-03",
+    currentStats: {
+        height: "58.4 cm",
+        weight: "6.0 kg",
+        age: "0歳4ヶ月"
+    },
+    likes: ["しましまぐるぐる"],
+    dislikes: ["ブロッコリー", "着替え"],
+    personality: "よだれが増えてきた。手をおしゃぶりがわりに吸うのが好き。"
+};
+
+export const achievementsData = [
+    {
+        id: "weight-3kg",
+        category: "body",
+        title: "体重が3kgを超えた",
+        date: "2025-09-19",
+        isUnlocked: true,
+        icon: "👶"
+    },
+    {
+        id: "weight-5kg",
+        category: "body",
+        title: "体重が5kgを超えた",
+        date: "2025-11-28",
+        isUnlocked: true,
+        icon: "👶"
+    },
+    {
+        id: "weight-7kg",
+        category: "body",
+        title: "体重が7kgを超えた",
+        date: "",
+        isUnlocked: false,
+        icon: "👶"
+    },
+    {
+        id: "first-sit",
+        category: "body",
+        title: "初めてお座りした",
+        date: "",
+        isUnlocked: false,
+        icon: "🧎‍♂️"
+    },
+    {
+        id: "first-haihai",
+        category: "body",
+        title: "初めてハイハイした",
+        date: "",
+        isUnlocked: false,
+        icon: "👣"
+    },
+    {
+        id: "first-standing",
+        category: "body",
+        title: "初めて立てた",
+        date: "",
+        isUnlocked: false,
+        icon: "👣"
+    },
+    {
+        id: "first-soft-talk",
+        category: "language",
+        title: "はじめての喃語",
+        date: "",
+        isUnlocked: false,
+        icon: "🗣️"
+    },
+    {
+        id: "baby-okayu",
+        category: "food",
+        title: "10倍粥を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🍙"
+    },
+    {
+        id: "baby-rice",
+        category: "food",
+        title: "米を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🍙"
+    },
+    {
+        id: "baby-carrot",
+        category: "food",
+        title: "にんじんを食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🥕"
+    },
+    {
+        id: "baby-potato",
+        category: "food",
+        title: "じゃがいもを食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🥔"
+    },
+    {
+        id: "baby-broccoli",
+        category: "food",
+        title: "ブロッコリーを食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🥦"
+    },
+    {
+        id: "baby-egg",
+        category: "food",
+        title: "卵黄を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🍳"
+    },
+    {
+        id: "baby-allegg",
+        category: "food",
+        title: "全卵を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🥚"
+    },
+    {
+        id: "baby-white-fish",
+        category: "food",
+        title: "白身魚を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🐟"
+    },
+    {
+        id: "baby-red-fish",
+        category: "food",
+        title: "赤身魚を食べた",
+        date: "",
+        isUnlocked: false,
+        icon: "🐟"
+    },
+    {
+        id: "sneeze-5",
+        category: "activity",
+        title: "くしゃみを5回連続でする",
+        date: "2025-10-30",
+        isUnlocked: true,
+        icon: "🤧"
+    },
+    {
+        id: "first-walk",
+        category: "activity",
+        title: "初めて歩いた",
+        date: "",
+        isUnlocked: false,
+        icon: "🚶‍♂️"
+    },
+    {
+        id: "first-run",
+        category: "activity",
+        title: "初めて走った",
+        date: "",
+        isUnlocked: false,
+        icon: "🏃‍♀️"
+    },
+    {
+        id: "first-spoon",
+        category: "activity",
+        title: "スプーンを使った",
+        date: "",
+        isUnlocked: false,
+        icon: "🥄"
+    },
+    {
+        id: "first-chopsticks",
+        category: "activity",
+        title: "お箸を使った",
+        date: "",
+        isUnlocked: false,
+        icon: "🥢"
+    },
+];
+
+export const memoriesData = [
+    {
+        id: 2026010601,
+        date: "2026-01-06",
+        title: "初詣",
+        description: "菅原神社に初詣に行ったよ",
+        tags: ["初詣"],
+        image: "src/assets/memories/20260106.jpg"
+    },
+    {
+        id: 2026010301,
+        date: "2026-01-03",
+        title: "4ヶ月記念日",
+        description: "4ヶ月経ったよ",
+        tags: ["記念日"],
+        image: "src/assets/memories/20260103.jpg"
+    },
+    {
+        id: 2026010101,
+        date: "2026-01-01",
+        title: "初めてのお正月",
+        description: "おじいちゃんおばあちゃんに会ってきたよ",
+        tags: ["イベント", "お正月"],
+        image: "src/assets/memories/20260101.jpg"
+    },
+    {
+        id: 2025122401,
+        date: "2025-12-24",
+        title: "初めてのクリスマス",
+        description: "おもちゃをプレゼントでもらったよ",
+        tags: ["イベント", "クリスマス"],
+        image: "src/assets/memories/20251224.jpg"
+    },
+    {
+        id: 2025122101,
+        date: "2025-12-21",
+        title: "お食い初め",
+        description: "おじいちゃんおばあちゃんとみんなでお食い初めをしたよ",
+        tags: ["イベント", "お食い初め"],
+        image: "src/assets/memories/20251221.jpg"
+    },
+    {
+        id: 2025121201,
+        date: "2025-12-12",
+        title: "100日記念日",
+        description: "生まれてから100日経ったよ",
+        tags: ["記念日"],
+        image: "src/assets/memories/20251212.jpg"
+    },
+    {
+        id: 2025121101,
+        date: "2025-12-11",
+        title: "動物園デビュー",
+        description: "ズーラシアに藤森家全員で行ってきたよ",
+        tags: ["お出かけ", "動物園"],
+        image: "src/assets/memories/20251211.jpg"
+    },
+    {
+        id: 2025120901,
+        date: "2025-12-09",
+        title: "結人くんとの戯れ",
+        description: "結人くんと結人くんママが遊びにきてくれたよ",
+        tags: ["いとこ"],
+        image: "src/assets/memories/20251209.jpg"
+    },
+    {
+        id: 2025120301,
+        date: "2025-12-03",
+        title: "3ヶ月記念日",
+        description: "3ヶ月経ったよ",
+        tags: ["記念日"],
+        image: "src/assets/memories/20251203.jpg"
+    },
+    {
+        id: 2025120201,
+        date: "2025-12-02",
+        title: "初めてのおもちゃ図書館",
+        description: "三輪あいこう保育園に行っておもちゃで遊んだよ",
+        tags: ["保育園"],
+        image: "src/assets/memories/20251202.jpg"
+    },
+    {
+        id: 2025112701,
+        date: "2025-11-27",
+        title: "初めてのつるっこ",
+        description: "つるっこに行って遊んできたよ",
+        tags: ["お出かけ"],
+        image: "src/assets/memories/20251127.jpg"
+    },
+    {
+        id: 2025112101,
+        date: "2025-11-21",
+        title: "まろに咬まれた",
+        description: "急に右足のすねをまろに咬まれて怪我したよ",
+        tags: ["ケガ"],
+        image: ""
+    },
+    {
+        id: 2025111602,
+        date: "2025-11-16",
+        title: "お風呂デビュー",
+        description: "沐浴を卒業して浴槽に浸かるようにしたよ",
+        tags: ["お風呂"],
+        image: ""
+    },
+    {
+        id: 2025111601,
+        date: "2025-11-16",
+        title: "澄乃ちゃんと対面",
+        description: "遠藤家が遊びにきてくれたよ",
+        tags: ["いとこ"],
+        image: "src/assets/memories/20251116.jpg"
+    },
+    {
+        id: 2025111401,
+        date: "2025-11-14",
+        title: "初めての予防接種",
+        description: "初めて注射を打ったよ",
+        tags: ["予防接種"],
+        image: "src/assets/memories/20251114.jpg"
+    },
+    {
+        id: 2025111201,
+        date: "2025-11-12",
+        title: "ギャラン反射を確認",
+        description: "ギャラン反射してたよ",
+        tags: ["反射"],
+        image: ""
+    },
+    {
+        id: 2025110301,
+        date: "2025-11-03",
+        title: "2ヶ月記念日",
+        description: "2ヶ月経ったよ",
+        tags: ["記念日"],
+        image: "src/assets/memories/20251103.jpg"
+    },
+    {
+        id: 2025102201,
+        date: "2025-10-22",
+        title: "結人くんと対面",
+        description: "藤森のおじいちゃんおばあちゃんのお家で対面したよ",
+        tags: ["いとこ"],
+        image: "src/assets/memories/20251022.jpg"
+    },
+    {
+        id: 2025101901,
+        date: "2025-10-19",
+        title: "初めての外食",
+        description: "お母さんのお友達と会いに新百合ヶ丘のファミレスにきたよ",
+        tags: ["外食"],
+        image: "src/assets/memories/20251019.jpg"
+    },
+    {
+        id: 2025101801,
+        date: "2025-10-18",
+        title: "律ちゃんと対面",
+        description: "いとこの律ちゃん家族がお家に来てくれたよ",
+        tags: ["いとこ"],
+        image: "src/assets/memories/20251018.jpg"
+    },
+    {
+        id: 2025101401,
+        date: "2025-10-14",
+        title: "初めての抱っこ紐散歩",
+        description: "抱っこ紐に入って初めてお散歩をしたよ",
+        tags: ["お散歩"],
+        image: "src/assets/memories/20251014.jpg"
+    },
+    {
+        id: 2025101301,
+        date: "2025-10-13",
+        title: "お宮参り",
+        description: "大国魂神社でお宮参りをしたよ",
+        tags: ["お宮参り"],
+        image: "src/assets/memories/20251013.jpg"
+    },
+    {
+        id: 2025101001,
+        date: "2025-10-10",
+        title: "1ヶ月検診",
+        description: "退院して初めてお医者さんに診てもらったよ",
+        tags: ["検診"],
+        image: ""
+    },
+    {
+        id: 2025100301,
+        date: "2025-10-03",
+        title: "1ヶ月記念日",
+        description: "生まれてから1ヶ月が経ったよ",
+        tags: ["記念日"],
+        image: "src/assets/memories/20251003.jpg"
+    },
+    {
+        id: 2025092601,
+        date: "2025-09-26",
+        title: "引越し",
+        description: "府中から町田の一軒家に引っ越したよ",
+        tags: ["引越し"],
+        image: ""
+    },
+    {
+        id: 2025091401,
+        date: "2025-09-14",
+        title: "おばあちゃんに抱っこ",
+        description: "お家に堀川のおじいちゃんとおばあちゃんが来てくれたよ",
+        tags: ["抱っこ", "祖父母"],
+        image: "src/assets/memories/20250914.jpg"
+    },
+    {
+        id: 2025091301,
+        date: "2025-09-13",
+        title: "おじいちゃんに抱っこ",
+        description: "お家に藤森のおじいちゃんとおばあちゃんが来て抱っこしてもらったよ",
+        tags: ["抱っこ", "祖父母"],
+        image: "src/assets/memories/20250913.jpg"
+    },
+    {
+        id: 2025091201,
+        date: "2025-09-12",
+        title: "退院",
+        description: "退院して初めてのお家に帰宅！まろとの出会い",
+        tags: ["退院", "まろ"],
+        image: "src/assets/memories/20250912.jpg"
+    },
+    {
+        id: 2025091101,
+        date: "2025-09-11",
+        title: "沐浴の練習",
+        description: "退院に向けてお父さんが沐浴を練習したよ",
+        tags: ["入院", "沐浴"],
+        image: "src/assets/memories/20250911.jpg"
+    },
+    {
+        id: 2025090901,
+        date: "2025-09-09",
+        title: "コットに移動",
+        description: "保育器から出て抱っこできるようになったよ",
+        tags: ["入院"],
+        image: "src/assets/memories/20250909.jpg"
+    },
+    {
+        id: 2025090701,
+        date: "2025-09-07",
+        title: "お母さんが面会",
+        description: "お母さんが退院して初めて面会にきたよ",
+        tags: ["面会", "入院"],
+        image: "src/assets/memories/20250907.jpg"
+    },
+    {
+        id: 2025090601,
+        date: "2025-09-06",
+        title: "ミルク飲み",
+        description: "お父さんが初めてミルクをあげたよ",
+        tags: ["ミルク", "入院"],
+        image: "src/assets/memories/20250906.jpg"
+    },
+    {
+        id: 2025090501,
+        date: "2025-09-05",
+        title: "初めての入院",
+        description: "新生児用救急車で1人小児医療センターへ搬送。",
+        tags: ["入院"],
+        image: "src/assets/memories/20250905.jpg"
+    },
+    {
+        id: 2025090401,
+        date: "2025-09-04",
+        title: "おじいちゃんおばあちゃんと面会",
+        description: "藤森の祖父母が面会にきたよ",
+        tags: ["面会"],
+        image: "src/assets/memories/20250904.jpg"
+    },
+    {
+        id: 2025090301,
+        date: "2025-09-03",
+        title: "誕生",
+        description: "午後0時01分に出産！",
+        tags: ["誕生日"],
+        image: "src/assets/memories/20250903.jpg"
+    }
+    // ,{
+    //     id: 0,
+    //     date: "2026-00-00",
+    //     title: "",
+    //     description: "",
+    //     tags: [""],
+    //     image: ""
+    // }
+];
+
+export const collectionsData = [
+    { id: 1, src: "https://www.toyroyal.co.jp/wp-content/uploads/2021/06/3187_01.jpg", title: "コロコロアレー", tags: ["おもちゃ"] },
+    { id: 2, src: "https://www.doshinsha.co.jp/nakami_imgs/9784494001019_0.jpg", title: "いないいないばあ", tags: ["絵本"] },
+    { id: 3, src: "https://shop.r10s.jp/quatresaisons-online/cabinet/0208/020802/g2303758-01.jpg", title: "ベビーボール", tags: ["おもちゃ"] },
+    { id: 4, src: "https://www.toyroyal.co.jp/wp-content/uploads/2024/03/6078_01.jpg", title: "モンポケ 7ステップメリージム", tags: ["おもちゃ"] },
+    { id: 5, src: "https://www.doshinsha.co.jp/nakami_imgs/9784494001361_0.jpg", title: "どんどこももんちゃん", tags: ["絵本"] },
+    { id: 6, src: "https://static.wixstatic.com/media/2de446_0186ed0fd2254bf99a6ed40322004c6d~mv2.jpg/v1/fill/w_1000,h_1334,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/2de446_0186ed0fd2254bf99a6ed40322004c6d~mv2.jpg", title: "コロン(COCO WALK)", tags: ["おもちゃ"] },
+    { id: 7, src: "https://www.watashinoheya.co.jp/resource/wh_item/g1303436-01.jpg", title: "ガラガラツインズ(エレーヌオーガニック)", tags: ["おもちゃ"] },
+    { id: 8, src: "https://familiar.co.jp/cdn/shop/products/180870_14.jpg?v=1678770495&width=1800", title: "ラトル(familiar)", tags: ["おもちゃ"] },
+    { id: 9, src: "https://image1.shopserve.jp/kurabokko.net/pic-labo/edointer-kororin_kumasan-7.jpg", title: "ころりんくまさん(エドインター)", tags: ["おもちゃ"] },
+    { id: 10, src: "https://image1.shopserve.jp/kurabokko.net/pic-labo/edointer-wonderLand-21.jpg", title: "ワンダーランド(エドインター)", tags: ["おもちゃ"] },
+    { id: 11, src: "https://hon.gakken.jp/img/books/352x452/1020311100.jpg", title: "しましまぐるぐる", tags: ["絵本"] },
+    { id: 12, src: "https://d21.co.jp/akachan-ehon/images/img_lineup_book_2.png", title: "もいもいボードブック", tags: ["絵本"] },
+    // { id: 13, src: "https://www.24028-net.jp/client_info/N24028/itemimage/4571138752971/4571138752971-7.jpg", title: "ベビーバウンサーエアES", tags: ["生活"] },
+    // { id: 0, src: "", title: "", tags: [""] },
+];
+
+import work1 from '../assets/works/work1.png';
+import work2 from '../assets/works/work2.png';
+import work3 from '../assets/works/work3.png';
+import work4 from '../assets/works/work4.png';
+
+export const worksData = [
+    { id: 1, src: work1, title: "初めての殴り書き", age: 0, date: "2024-12-10" },
+    { id: 2, src: work2, title: "手形アート", age: 1, date: "2025-05-15" },
+    { id: 3, src: work3, title: "謎の生物", age: 1, date: "2025-08-20" },
+    { id: 4, src: work4, title: "ママの顔", age: 2, date: "2026-06-01" },
+];
+
