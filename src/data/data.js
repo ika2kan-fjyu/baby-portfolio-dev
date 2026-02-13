35f6a0766ef2a12f4e4d0752253952438ccc91b0
ika2kan-fjyu <yu3horikawa@gmail.com>
2026-02-13 20:14:44 +0900

    worksをコメントアウト

--- src/data/data.js
+++ src/data/data.js
@@ -627,9 +627,9 @@ import work4 from '/works/work4.png';
 // 
 
 export const worksData = [
-    { id: 1, src: work1, title: "初めての殴り書き", age: 0, date: "2024-12-10" },
-    { id: 2, src: work2, title: "手形アート", age: 1, date: "2025-05-15" },
-    { id: 3, src: work3, title: "謎の生物", age: 1, date: "2025-08-20" },
-    { id: 4, src: work4, title: "ママの顔", age: 2, date: "2026-06-01" },
+    // { id: 1, src: work1, title: "初めての殴り書き", age: 0, date: "2024-12-10" },
+    // { id: 2, src: work2, title: "手形アート", age: 1, date: "2025-05-15" },
+    // { id: 3, src: work3, title: "謎の生物", age: 1, date: "2025-08-20" },
+    // { id: 4, src: work4, title: "ママの顔", age: 2, date: "2026-06-01" },
 ];
 
