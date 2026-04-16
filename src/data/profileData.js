b09d307adb92ee8efededf1cbfb7f81e58f2ca12
ika2kan-fjyu <yu3horikawa@gmail.com>
2026-04-16 16:05:30 +0900

    プロフィールを更新

--- src/data/profileData.js
+++ src/data/profileData.js
@@ -2,11 +2,11 @@ export const profileData = {
     name: "周 (あまね/Amane)",
     birthDate: "2025-09-03",
     currentStats: {
-        height: { value: "66.5cm", date: "2026-03-05" },
-        weight: { value: "6.3kg", date: "2026-03-05" },
+        height: { value: "67.2cm", date: "2026-04-15" },
+        weight: { value: "6.7kg", date: "2026-04-15" },
     },
     likes: ["しましまぐるぐる", "男前豆腐"],
-    dislikes: ["トマト🍅", "ペットボトルを潰す音", "鼻吸い", "顔の保湿"],
+    dislikes: ["トマト🍅", "ほうれん草🥬", "ペットボトルを潰す音", "鼻吸い", "顔の保湿"],
     clothesSize: "70 ~ 80cm",
     shoeSize: "??? cm",
     personality: "よく笑う。知らない人の顔を見ると泣き出したりして、少し人見知りのような素振りがある。"
